fd3383595c48a19c10551ce2592db473ceb27f46
Xiang Li <51950425+leexx227@users.noreply.github.com>
2019-07-18 11:47:54 +0800

    Delete SQLUnitTestStartUp.sql

--- HighAvailabilityModule.UnitTest.SQL/SQLUnitTestStartUp.sql
+++
@@ -1,9 +0,0 @@
-EXEC sp_configure 'clr enabled', 1;
-RECONFIGURE;
-GO
-EXEC sp_configure 'clr enabled'
-
-USE HighAvailabilityWitness
-GO
-ALTER DATABASE HighAvailabilityWitness SET TRUSTWORTHY ON;
-
